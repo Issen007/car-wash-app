be2211d1b99be5f72d60de62e0ed1569f0ddba88
Christian Petersson <Christian.Petersson@isstech.io>
2022-10-05 12:49:23 +0200

    Fix Static Root

--- src/webserver/settings.py
+++ src/webserver/settings.py
@@ -143,13 +143,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+if DEBUG:
+    print('### DEBUG MODE ###')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+    MEDIA_ROOT = os.path.join(os.environ.get('REPO_DIR', ''),'media_root')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
